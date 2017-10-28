dad26bf710539c328b183f1be1f5baafa90431f6
cristian vidal <cristian.vidal.lopez@gmail.com>
2017-10-28 18:53:15 -0300

    ADD: methods of the controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,21 @@
 class TodosController < ApplicationController
 end
+
+def new
+  @todos = Todo.new
+end
+
+def index
+
+end
+
+def create
+  todos = Todo.new(params:[todo_params])
+  redirect_to todo_index_path
+end
+
+private
+
+def todo_params
+  params.require(:todo).permit(:detail, :completed)
+end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,4 @@
 class Todo < ApplicationRecord
+  validates :detail, uniqueness: true
+  validates :completed, presence: true
 end
