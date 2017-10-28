32084a570975d42778e735c0f9037524063fb480
cristian vidal <cristian.vidal.lopez@gmail.com>
2017-10-28 20:20:05 -0300

    FIX: problem with routes, Start using resources in routes.rb

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,25 +1,34 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
 
-def index
-@todos = Todo.all
-end
+  def new
+    @todo = Todo.new
+  end
 
-def new
-  @todo = Todo.new
-end
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
 
-def create
-  @todo = Todo.new(todo_params)
-  @todo.save
-  redirect_to todos_path
-end
+  def show
+    @todo = Todo.find(params[:id])
+  end
 
-def show
-end
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    redirect_to todos_path
+  end
 
   private
-   def todo_params
-     params.require(:todo).permit(:description, :completed)
-   end
 
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'todos', to: 'todos#index'
-  get 'todos/new', to: 'todos#new'
-  post 'todos', to: 'todos#create'
-
+  resources :todos
+  # get 'todos', to: 'todos#index'
+  # get 'todos/new', to: 'todos#new'
+  # post 'todos', to: 'todos#create'
+  # get 'todos', to: 'todos#show', as: 'todo'
+  # get 'todos/:id/edit', to: 'todos#edit'
+  # patch 'todos/:id', to: 'todos#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
